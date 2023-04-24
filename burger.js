07474d0c13cb703838d2cd68cf15f15579c6630c
valentinats <108477319+valentinats@users.noreply.github.com>
2023-04-24 22:08:30 +0300

    Added burger menu

--- burger.js
+++ burger.js
@@ -1,6 +1,8 @@
 const BURGER_OPENED_CLASSNAME = 'burger_open';
 const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn_open';
+const BODY_FIXED_CLASSNAME = 'body_fixed';
 
+const bodyNode = document.querySelector('body');
 const burgerNode = document.querySelector('.js-burger');
 const burgerBtnNode = document.querySelector('.js-burger-btn');
 const burgerContentNode = document.querySelector('.js-burger__content')
